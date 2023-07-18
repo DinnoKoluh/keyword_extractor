08aa09b89247b2599a0f9314b93f3d919373ef1a
DinnoKoluh <koluh.dinno@gmail.com>
2023-07-19 00:41:45 +0200

    Fixed line spacing in LaTeX file to 1.5. Sentence splitting for co-occurrence needs. Implemented co-occurrence matrix generation.

--- KeywordExtractor.py
+++ KeywordExtractor.py
@@ -1,7 +1,16 @@
 from utils import *
+import networkx as nx
+# https://networkx.org/documentation/stable/tutorial.html
 
 class KeywordExtractor:
     def __init__(self, abstract):
-        self.tokens = prune_text(abstract)
+        self.abstract = abstract # raw input text
+        self.tokens, self.sentences = prune_text(abstract) # list of tokens and sentences 
+        self.unique_tokens = list(set(self.tokens))
+        self.graph = nx.Graph() # empty graph structure where 
+        self.graph.add_nodes_from(self.tokens) # adding list of tokens as graph vertices
+
+
+
 
     
--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ from nltk.tokenize import word_tokenize, MWETokenizer
 from nltk.corpus import stopwords
 import lexicons
 import re
+from gensim.models import Word2Vec
+from itertools import combinations
+import numpy as np
 
 #nltk.download("stopwords")
 
@@ -15,13 +18,22 @@ def prune_text(text):
     tokens = join_MWE(tokens) # joining upper-case tokens into MWE
     tokens = mwe_tokenizer.tokenize(tokens) # tokenizing text
     #pos_tags = nltk.pos_tag(tokens) # PoS tagging (nouns and adjectives)
-    # TODO: custom stopwords
+    tokens = [token.lower() for token in tokens]
     stop_words = set(stopwords.words('english')) # removing stopwords
     tokens = [token for token in tokens if token.lower() not in stop_words]
-
+    # extracting sentences from input text
+    sentences = []
+    sentence = []
+    for token in tokens:
+        sentence.append(token)
+        if token in ['.', '?', '!']:
+            sentences.append(sentence[0:-1])
+            sentence = []
+            continue
+    
+    # TODO: custom stopwords
     tokens = [token for token in tokens if re.match(r'^[a-zA-Z0-9_-]+$', token)] # removing non-alphanumeric tokens
-
-    return tokens
+    return tokens, sentences
 
 def load_abstract(name):
     with open('data/' + name + '.txt', 'r') as file:
@@ -40,7 +52,7 @@ def join_MWE(tokens):
             mw_token = tokens[i]
             i = i + 1
             flag = False
-            while tokens[i][0].isupper() and i < len(tokens): # while the next token is uppercase and the iterator is less then the length of the number of tokens
+            while i < len(tokens) and tokens[i][0].isupper(): # while the next token is uppercase and the iterator is less then the length of the number of tokens
                 flag = True
                 mw_token = mw_token + '_' + tokens[i]
                 i = i + 1
@@ -52,3 +64,36 @@ def join_MWE(tokens):
         out_tokens.append(tokens[i])
         i = i + 1
     return out_tokens
+
+def get_co_matrix(sentences, unique_tokens, window_size=3):
+    """
+    Calculates co-occurrence matrix for a list of sentences. Choose appropriate window-size
+    for your relevant needs.
+    """
+    n = len(unique_tokens)
+    # Making a dictionary of indexes corresponding to tokens which are also key values. Needed for fast list access.
+    # Dictionary is in the form: {token0: 0, token1: 1, ...} 
+    index_dict = {key: index for index, key in enumerate(unique_tokens)} 
+    co_matrix = np.zeros(shape=(n,n))
+    def populate_co_matrix(co_matrix, index_dict, window_tokens):
+        pairs = list(combinations(window_tokens, 2)) # all pair combinations of a list (ex. [1, 2, 3] => (1, 2), (1, 3), (2,3))
+        for pair in pairs:
+            t1, t2 = pair
+            co_matrix[index_dict[t1]][index_dict[t2]] += 1
+            co_matrix[index_dict[t2]][index_dict[t1]] += 1
+        return co_matrix    
+    
+    for sentence in sentences:
+        short_sentence = True # in case of sentences that are shorter than the window size
+        for i, _ in enumerate(sentence):
+            if i + window_size > len(sentence):
+                break
+            short_sentence = False # means that the sentence is longer than the window size
+            window_tokens = sentence[i:i+window_size]
+            print(window_tokens)
+            co_matrix = populate_co_matrix(co_matrix, index_dict, window_tokens)
+        if short_sentence:
+            print(sentence) 
+            co_matrix = populate_co_matrix(co_matrix, index_dict, sentence)
+    return co_matrix, index_dict
+
