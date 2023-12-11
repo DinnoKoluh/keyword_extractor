1352adb6d0ab173fdd038ef4ba17278b2ecf5d8d
DinnoKoluh <koluh.dinno@gmail.com>
2023-12-12 00:21:48 +0100

    finished presentation

--- lexicons.py
+++ lexicons.py
@@ -3,4 +3,4 @@ stopwords_lowercase = []
 stopwords_uppercase = [string.capitalize() for string in stopwords_lowercase]
 
 mwe_list = [('New', 'York'), ('United', 'States'), ('machine', 'learning'), ('Stochastic', 'Gradient', 'Descent'), ('deep', 'learning'), ('World', 'War', '2'),
-            ('Natural', 'Language', 'Processing'), ('neural', 'network'), ('image', 'recognition')]
+            ('Natural', 'Language', 'Processing'), ('neural', 'network'), ('image', 'recognition'), ('word', 'embeddings')]
